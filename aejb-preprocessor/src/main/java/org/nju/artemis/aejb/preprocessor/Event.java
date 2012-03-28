f3d913464091b43d8defe27fa27a88c94baa7652
suping <njupsu@gmail.com>
2012-03-28 10:38:03 +0800

    modify recognize_state & code format

--- aejb-preprocessor/src/main/java/org/nju/artemis/aejb/preprocessor/Event.java
+++ aejb-preprocessor/src/main/java/org/nju/artemis/aejb/preprocessor/Event.java
@@ -7,15 +7,15 @@ public class Event {
 	 */
 	private int tail;
 	private String event;
-	
+
 	public Event(int head, int tail, String event) {
 		super();
 		this.head = head;
 		this.tail = tail;
 		this.event = event;
-		System.out.println(head+"-"+event+"-"+tail);
+		System.out.println(head + "-" + event + "-" + tail);
 	}
-	
+
 	/**
 	 * @param head
 	 * @param tail
@@ -24,45 +24,51 @@ public class Event {
 	public int getHead() {
 		return head;
 	}
+
 	/**
-	 * @param head the head to set
+	 * @param head
+	 *            the head to set
 	 */
 	public void setHead(int head) {
 		this.head = head;
 	}
+
 	/**
 	 * @return the tail
 	 */
 	public int getTail() {
 		return tail;
 	}
+
 	/**
-	 * @param tail the tail to set
+	 * @param tail
+	 *            the tail to set
 	 */
 	public void setTail(int tail) {
 		this.tail = tail;
 	}
+
 	/**
 	 * @return the event
 	 */
 	public String getEvent() {
 		return event;
 	}
+
 	/**
-	 * @param event the event to set
+	 * @param event
+	 *            the event to set
 	 */
 	public void setEvent(String event) {
 		this.event = event;
 	}
-	public String getPort(){
-		if(event.contains("Ejb"))
-		{
-			return event.split("\\.")[1];			
+
+	public String getPort() {
+		if (event.contains("Ejb")) {
+			return event.split("\\.")[1];
 		}
 		return null;
-		
-	}
-	
 
+	}
 
 }
